8f82fb15ed438566d1f230e3c7cb3c304991f797
akrz <akrz>
2015-03-08 20:57:05 +0000

    Changing identation to spaces

--- client/layout/layout.js
+++ client/layout/layout.js
@@ -1,13 +1,10 @@
 Meteor.call('getMatch', function(error, result){
     var duration = numeral(result).format('00:00:00');
     Session.set('match', duration);
-    /*Meteor.call('secondsToHms', result, function (error, result) {
-        	
-    });*/
 });
 
 Template.layout.helpers({
-	match: function () {
-		return Session.get('match');
-	}
+    match: function () {
+        return Session.get('match');
+    }
 });
--- packages/dotaApi/dota.js
+++ packages/dotaApi/dota.js
@@ -1,25 +1,25 @@
 Dota = {};
 
 Dota.getMatch = function() {
-	if (!Meteor.settings.steamToken)
-		throw new Meteor.Error(500, 'Enter a valid Steam Token in Meteor.settings');
+    if (!Meteor.settings.steamToken)
+        throw new Meteor.Error(500, 'Enter a valid Steam Token in Meteor.settings');
 
-	var matchResponse = Meteor.http.get(
-		"https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?",
-		{
-			params:{
-				"match_id": "1305454585",
-				"key": Meteor.settings.steamToken
-			}
-		}
-	);
+    var matchResponse = Meteor.http.get(
+        "https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?",
+        {
+            params:{
+                "match_id": "1305454585",
+                "key": Meteor.settings.steamToken
+            }
+        }
+    );
 
-	if (matchResponse.statusCode === 200) {
-		return matchResponse.data.result.duration
-	}
-	else {
-		throw new Meteor.Error(500, "getMatch failed with error: "+matchResponse.statusCode);
-	}
+    if (matchResponse.statusCode === 200) {
+        return matchResponse.data.result.duration
+    }
+    else {
+        throw new Meteor.Error(500, "getMatch failed with error: "+matchResponse.statusCode);
+    }
 }
 
 Meteor.methods({
